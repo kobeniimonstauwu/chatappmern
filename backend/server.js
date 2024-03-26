fc1b78ac566caef5dcc0d1f8664e684c34077733
unknown <Orion>
2024-03-26 17:37:21 +0800

    First Part Completed - Backend

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import connectToMongoDB from './db/connectToMongoDB.js'
+
+
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+
+// The Middlewares are "app.use"
+
+app.use(express.json()) //Parses json payloads (From req.body)
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
+app.get("/", (req, res) => {
+    //root route http://locahost:5000/
+    res.send("Hello Worlds")
+})
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server running on port ${PORT}`)
+})
